3f0d55eba7bed3dd68418701575b126befa62f86
Anderson Exner <andersonexner@gmail.com>
2023-09-26 16:23:58 -0300

    ajusta remove departamento

--- Capitulo02/Controllers/DepartamentoController.cs
+++ Capitulo02/Controllers/DepartamentoController.cs
@@ -130,7 +130,6 @@ namespace Capitulo02.Controllers
         public async Task<IActionResult> DeleteConfirmed(long? id)
         {
             var departamento = await departamentoDal.EliminarDepartamentoPorId((long) id);
-            _context.Departamentos.Remove(departamento);
             TempData["Message"] = "Departamento " + departamento.Nome.ToUpper() + " foi removido";
             return RedirectToAction(nameof(Index));
         }
