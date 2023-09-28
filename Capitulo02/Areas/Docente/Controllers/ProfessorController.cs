5d961f405a9c63279aa990fd86a6ce2503058541
Anderson Exner <andersonexner@gmail.com>
2023-09-28 20:20:48 -0300

    feature: insere views e controllores da classe professores

--- Capitulo02/Areas/Docente/Controllers/ProfessorController.cs
+++ Capitulo02/Areas/Docente/Controllers/ProfessorController.cs
@@ -2,6 +2,9 @@
 using Capitulo02.Data.DAL.Docente;
 using Capitulo02.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Modelo.Docente;
+using Modelo.Cadastros;
 
 namespace Capitulo02.Areas.Docente.Controllers
 {
@@ -21,6 +24,117 @@ namespace Capitulo02.Areas.Docente.Controllers
             departamentoDAL = new DepartamentoDAL(context);
             cursoDAL = new CursoDAL(context);
             professorDAL = new ProfessorDAL(context);
+        }
+
+        private async Task<bool> ProfessorExist(long? id)
+        {
+            return await professorDAL.ObterProfessorPorId((long)id) != null;
+        }
+
+        //OBTER VIEW POR IP
+        private async Task<IActionResult> ObterVisaoProfessorPorId(long? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var professor = await professorDAL.ObterProfessorPorId((long)id);
+            if(professor == null)
+            {
+                return NotFound();
+            }
+
+            return View(professor);
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await professorDAL.ObterProfessorPorNome().ToListAsync());
+        }
+
+        //CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Nome")] Professor professor)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    await professorDAL.GravarProfessor(professor);
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch (DbUpdateException)
+            {
+                ModelState.AddModelError("", "Não foi possível inserir dados.");
+            }
+            return View(professor);
+        }
+
+        //EDIT
+        public async Task<IActionResult> Edit(long? id)
+        {
+            return await ObterVisaoProfessorPorId(id);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long? id, [Bind("ProfessorID", "Nome")] Professor professor)
+        {
+            if(id != professor.ProfessorID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await professorDAL.GravarProfessor(professor);
+                }
+                catch
+                {
+                    if(!await ProfessorExist(professor.ProfessorID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(professor);
+        }
+
+        //DETAIS
+        public async Task<IActionResult> Details(long? id)
+        {
+            return await ObterVisaoProfessorPorId(id);
+        }
+
+        //DELETE
+
+        public async Task<IActionResult> Delete(long? id)
+        {
+            return await ObterVisaoProfessorPorId(id);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long? id)
+        {
+            var inst = await professorDAL.RemoverProfessorPorId((long)id);
+            TempData["Message"] = "Professor " + inst.Nome.ToUpper() + " foi removido.";
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- Capitulo02/Data/DAL/Docente/ProfessorDAL.cs
+++ Capitulo02/Data/DAL/Docente/ProfessorDAL.cs
@@ -1,4 +1,7 @@
-﻿namespace Capitulo02.Data.DAL.Docente
+﻿using Microsoft.EntityFrameworkCore;
+using Modelo.Docente;
+
+namespace Capitulo02.Data.DAL.Docente
 {
     public class ProfessorDAL
     {
@@ -8,5 +11,37 @@
         {
             _context = context;
         }   
+
+        public IQueryable<Professor> ObterProfessorPorNome()
+        {
+            return _context.Professores.OrderBy(p => p.Nome);
+        }
+
+        public async Task<Professor> ObterProfessorPorId(long id)
+        {
+            return await _context.Professores.SingleOrDefaultAsync(p => p.ProfessorID == id);
+        }
+
+        public async Task<Professor> GravarProfessor(Professor professor)
+        {
+            if(professor.ProfessorID == null)
+            {
+                _context.Professores.Add(professor);
+            }
+            else
+            {
+                _context.Update(professor);
+            }
+            await _context.SaveChangesAsync();
+            return professor;
+        }
+
+        public async Task<Professor> RemoverProfessorPorId(long id)
+        {
+            Professor professor = await ObterProfessorPorId(id);
+            _context.Professores.Remove(professor);
+            await _context.SaveChangesAsync();
+            return professor;
+        }
     }
 }
